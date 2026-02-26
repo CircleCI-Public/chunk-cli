645a028f57ef6affbe6668cba88272bf139b8be2
Hanabel M. <hanabel.mengistu@circleci.com>
2026-02-26 09:18:59 -0800

    Migrate to commander

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,13 +1,7 @@
 import packageJson from "../../package.json";
-import { DEFAULT_MODEL } from "../config";
 import type { CommandResult } from "../types";
-import { bold, dim } from "../ui/colors";
-import { printBanner } from "../ui/logo";
 
 export async function runVersion(): Promise<CommandResult> {
-	printBanner([
-		`${bold("chunk")} ${dim(`v${packageJson.version}`)}`,
-		dim(`Default model: ${DEFAULT_MODEL}`),
-	]);
+	console.log(`chunk ${packageJson.version}`);
 	return { exitCode: 0 };
 }
