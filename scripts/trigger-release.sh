eb60e2cdbf84eb9fd73330dc4a26bed505ddccf3
Robert Zuber <rob@zuber.net>
2026-02-28 01:57:28 +0000

    add release script

---
+++ scripts/trigger-release.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CIRCLECI_TOKEN="${CIRCLECI_TOKEN:?CIRCLECI_TOKEN is required}"
+
+PROJECT_SLUG="github/CircleCI-Public/chunk-cli"
+BRANCH="z00b/releaser"
+DEFINITION_ID="7a093425-e5d7-4d4e-b178-d049d0c35f0d"
+
+curl -sSL \
+  --request POST \
+  --url "https://circleci.com/api/v2/project/${PROJECT_SLUG}/pipeline/run" \
+  --header "Circle-Token: ${CIRCLECI_TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data "{
+    \"checkout\": {\"branch\": \"${BRANCH}\"},
+    \"config\": {\"branch\": \"${BRANCH}\"},
+    \"definition_id\": \"${DEFINITION_ID}\"
+  }"
