72342bd336b301999f9f718c181ef3775b78df17
Ryan E. Hamilton <ryan.hamilton@circleci.com>
2026-05-27 18:31:34 -0400

    experiment: task 2

--- internal/racefixture/racefixture.go
+++ internal/racefixture/racefixture.go
@@ -1,5 +1,8 @@
 package racefixture
 
+// lintTrap is an unused variable that will fail golangci-lint unused checks
+var lintTrap = 1
+
 // Sum returns the sum of a and b.
 func Sum(a, b int) int {
 	return a + b
