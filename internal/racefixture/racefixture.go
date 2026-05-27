4b73e5d6d76325e6dd5491489fc63d9c8c9b1c44
Ryan E. Hamilton <ryan.hamilton@circleci.com>
2026-05-27 18:20:58 -0400

    experiment: task 7

--- internal/racefixture/racefixture.go
+++ internal/racefixture/racefixture.go
@@ -1,8 +1,9 @@
 package racefixture
 
 // Sum returns the sum of a and b.
-func Sum(a, b int) int {
-	return a + b
+func Sum(a, b int) int
+{
+	return a+b
 }
 
 // Multiply returns the product of a and b.
