cabf1f19e2cb7be623130401e2c239539983ee2b
webster <michael@webster.fyi>
2026-06-01 20:10:56 -0400

    test: remove deliberately failing test

--- internal/gitutil/gitutil_test.go
+++ internal/gitutil/gitutil_test.go
@@ -11,10 +11,6 @@ import (
 	"gotest.tools/v3/assert"
 )
 
-func TestDeliberateFailure(t *testing.T) {
-	t.Error("this test is supposed to fail")
-}
-
 func setupRepo(t *testing.T) string {
 	t.Helper()
 	dir := t.TempDir()
