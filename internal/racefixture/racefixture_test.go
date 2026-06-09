daa4de73ee1197e4160b6b2f8fbc97b15e5f2e28
Ryan E. Hamilton <ryan.hamilton@circleci.com>
2026-06-09 13:45:49 -0400

    place failure in `Sum` function

--- internal/racefixture/racefixture_test.go
+++ internal/racefixture/racefixture_test.go
@@ -3,7 +3,7 @@ package racefixture
 import "testing"
 
 func TestSum(t *testing.T) {
-	if got := Sum(1, 2); got != 99 {
+	if got := Sum(1, 2); got != 3 {
 		t.Fatalf("Sum(1, 2) = %d, want 3", got)
 	}
 }
